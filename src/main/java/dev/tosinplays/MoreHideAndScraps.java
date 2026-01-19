d77aa232aa5992081a483b388bfecd7ba3998d19
Oluwatosin Awofeso <oluwatosin.awofeso@gennev.com>
2026-01-19 15:46:35 -0600

    main(main): set to nothing

---
+++ src/main/java/dev/tosinplays/MoreHideAndScraps.java
@@ -0,0 +1,19 @@
+package dev.tosinplays;
+
+import com.hypixel.hytale.server.core.event.events.player.PlayerReadyEvent;
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+
+import javax.annotation.Nonnull;
+
+public class MoreHideAndScraps extends JavaPlugin {
+
+    public MoreHideAndScraps(@Nonnull JavaPluginInit init) {
+        super(init);
+    }
+
+    @Override
+    protected void setup() {
+
+    }
+}
